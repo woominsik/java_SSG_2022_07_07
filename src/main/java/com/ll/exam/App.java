3773211ea741c312b87952f95ac866404b0addd6
woominsik <9737638@naver.com>
2022-07-07 15:41:31 +0900

    등록기능 구현 시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,37 +3,29 @@ package com.ll.exam;
 import java.util.Scanner;
 
 public class App {
-    public void run(){
+    public void run() {
         System.out.println("== 명언 SSG ==");
 
         Scanner sc = new Scanner(System.in);
-        int sayCount=1;
-        while(true){
-            boolean outerBreak =false;
+
+        outer:
+        while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
-            switch (cmd){
-                case "종료":
-                    outerBreak = true;
-                    break ;
 
-                case "등록" :
-                    등록();
-                    System.out.println(sayCount+"번 명언이 등록되었습니다.");
-                    sayCount++;
+            switch (cmd) {
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+                    System.out.println("1번 명언이 등록되었습니다.");
                     break;
-            }
-            if(outerBreak){
-                break;
+                case "종료":
+                    break outer;
             }
         }
-    }
-    static void 등록(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("명언 : ");
-        String say =sc.nextLine();
-        System.out.print("작가 : ");
-        String person = sc.nextLine();
 
+        sc.close();
     }
 }
