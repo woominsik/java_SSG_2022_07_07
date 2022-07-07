db510a3d73b402850c32ffb9a6c67b29d9d26996
woominsik <9737638@naver.com>
2022-07-07 15:26:03 +0900

    명령 프롬프트 생성 및 종료 기능 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,27 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
     public void run(){
+        System.out.println("== 명언 SSG ==");
 
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            boolean outerBreak =false;
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine().trim();
+            switch (cmd){
+                case "종료":
+                    outerBreak = true;
+                    break ;
+
+
+            }
+            if(outerBreak){
+                break;
+            }
+        }
     }
+
 }
