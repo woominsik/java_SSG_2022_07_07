679e60b4651be147a84960d9f20ecdf113cd5068
woominsik <9737638@naver.com>
2022-07-08 14:16:47 +0900

    íinputStream을 통한 테스트데이터 입력

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,7 +1,37 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @Test
+    public void 테스트_실험(){
+        int rs = 10+20;
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 테스트_스캐너(){
+        String input= """
+            등록
+            명언1
+            작가1
+            """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals(cmd,"등록");
+        assertEquals(content,"명언1");
+        assertEquals(author,"작가1");
+    }
 }
